0d834745be60f70edd262eadec9dc621144c3201
Brandon Pinkerton <brandon.a.pinkerton@gmail.com>
2022-09-27 14:26:51 -0500

    added Hello World example

---
+++ week1/day2/Hello.java
@@ -0,0 +1,27 @@
+public class Hello {
+	// single-line comment
+	// information goes in here
+
+
+	// this is a method that belongs to the Hello class
+	// a method is a repeatable action that you want to perform
+
+	// The main method is special in Java, every program needs one
+	// it is the entry point of the application
+	public static void main(String[] args){
+		// This is a method invocation
+		System.out.println("Hello.");
+
+		// variables -> are portable, reusable values
+		// <data type> <name of the variable>
+		String name = "Brandon";
+
+		
+		System.out.println(name);
+		name + " is great.";
+		System.out.println(name);
+	}
+
+
+	
+}
